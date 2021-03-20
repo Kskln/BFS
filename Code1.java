5492fa8465ca960a60308e12062376cabbaa8c71
180103059 <49470947+180103059@users.noreply.github.com>
2021-03-21 00:33:09 +0600

    Update Code1.java

--- Code1.java
+++ Code1.java
@@ -1,1 +1,16 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
+class Graph {
+  ArrayList<ArrayList<Inтeger>>graph;
+  int V;
+  Graph(int nodes){
+    V=nodes;
+    graph=new ArrayList<ArrayList<Integer>>();
+    for(int i=0; i<V; i++){
+      graph.add(new ArrayList<Integer>());
+    }
+  }
+  
+  
