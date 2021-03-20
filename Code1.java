2d8467471b5c8e0ffdd2f011fde32621c1439458
180103037 <80393702+180103037@users.noreply.github.com>
2021-03-21 01:00:08 +0600

    Update Code1.java

--- Code1.java
+++ Code1.java
@@ -44,6 +44,19 @@ class Graph {
 }
 
         
-                      
+public class Graph{
+  public static void main (String args[]){
+    Graph g = new Graph(5);
+    g.addEdge(0,1);
+    g.addEdge(3,2);
+    g.addEdge(2,4);
+    g.addEdge(1,4);
+    g.addEdge(3,1);
+    g.addEdge(2,0);
+    g.printGraph();
+    System.out.println();
+    g.BFS(0);
+  }
+}
   
   
