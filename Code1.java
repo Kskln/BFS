c1c8fa272f36cc35bd90d6f8f4d4ce0787565e0d
180103059 <49470947+180103059@users.noreply.github.com>
2021-03-21 00:52:44 +0600

    Update Code1.java

--- Code1.java
+++ Code1.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Graph {
-  ArrayList<ArrayList<Inтeger>>graph;
+  ArrayList<ArrayList<Integer>>graph;
   int V;
   Graph(int nodes){
     V=nodes;
